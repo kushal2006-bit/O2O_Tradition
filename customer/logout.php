a136259e9aa58432f86236546923c830afe2443a
kushal2006-bit <patilkushal121@gmail.com>
2026-09-23 03:52:37 +0530

    Harden customer and vendor logout session cleanup

--- customer/logout.php
+++ customer/logout.php
@@ -1,5 +1,16 @@
 <?php
+session_set_cookie_params([
+    'httponly' => true,
+    'secure' => (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off'),
+    'samesite' => 'Lax'
+]);
 session_start();
+$_SESSION = [];
+if (ini_get('session.use_cookies')) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+}
 session_destroy();
 header('Location: login.php');
 exit;
+?>
