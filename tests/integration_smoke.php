46684ac8990c62795a3c7260bbf7e756b08a35b5
kushal2006-bit <patilkushal121@gmail.com>
2026-09-25 15:44:22 +0530

    Cover payment webhook idempotency

--- tests/integration_smoke.php
+++ tests/integration_smoke.php
@@ -170,6 +170,8 @@ requireText('customer/account.php', 'request_deletion');
 requireText('admin/deletion_requests.php', 'Approve & Anonymize');
 requireText('customer/complete_look.php', 'Book rental');
 requireText('customer/complete_look.php', 'Buy outfit');
+requireText('database/migrations/034_payment_webhook_idempotency.sql', 'payment_webhook_events');
+requireText('customer/payment_webhook.php', 'X_RAZORPAY_EVENT_ID');
 requireText('database/migrations/032_customer_email_otp.sql', 'otp_token_hash');
 requireText('shared/email.php', 'o2oCreateLoginOtp');
 requireText('customer/login.php', 'otp_login');
